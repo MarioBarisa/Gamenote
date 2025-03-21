d09b073ae3b24b9786f8159563e5db6a27a595bb
MarioBarisa <130148782+MarioBarisa@users.noreply.github.com>
2025-03-21 10:33:05 +0000

    Opis promene

---
+++ Gamenote/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+
+<template>
+
+<div class="w-full h-full flex flex-col justify-start bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+  
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
